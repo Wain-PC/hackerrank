defe8bb73782523a73d13c69aa3921998a6d3557
Wain-PC <wain-pc@mail.ru>
2018-12-14 17:19:29 +0300

    Hash Tables/Two_Strings

---
+++ Hash Tables/Two_Strings.js
@@ -0,0 +1,9 @@
+const solve = (s1, s2) => {
+  const w1Hash = s1.split('').reduce((acc, letter) => {
+    acc[letter] = true;
+    return acc;
+  }, {});
+  return s2.split('').some(letter => w1Hash[letter]) ? 'YES' : 'NO';
+};
+
+module.exports = { solve };
---
+++ Hash Tables/Two_Strings.test.js
@@ -0,0 +1,10 @@
+const { solve } = require('./Two_Strings');
+
+describe('Ransom_Note', () => {
+  it('should solve default example 1', () => {
+    expect(solve('hello', 'world')).toBe('YES');
+  });
+  it('should solve default example 1', () => {
+    expect(solve('hi', 'world')).toBe('NO');
+  });
+});
