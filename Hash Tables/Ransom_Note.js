718289ce23d3505b7c5cbdff0ee31015f29925d1
Wain-PC <wain-pc@mail.ru>
2018-12-14 17:13:05 +0300

    Hash Tabled/Ransom_Note

---
+++ Hash Tables/Ransom_Note.js
@@ -0,0 +1,18 @@
+const solve = (magazine, note) => {
+  const magHash = magazine.reduce((acc, word) => {
+    if (!acc[word]) {
+      acc[word] = 0;
+    }
+    acc[word]++;
+    return acc;
+  }, {});
+  return note.every((word) => {
+    if (!magHash[word]) {
+      return false;
+    }
+    magHash[word]--;
+    return true;
+  }) ? 'Yes' : 'No';
+};
+
+module.exports = { solve };
