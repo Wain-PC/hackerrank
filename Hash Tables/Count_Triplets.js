424b1254dd9bee4ea9977647f5fe1f4a98855961
Wain-PC <wain-pc@mail.ru>
2018-12-19 15:05:45 +0300

    Hash Tables/Count_Triplets

---
+++ Hash Tables/Count_Triplets.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-prototype-builtins */
+const solve = (arr, r) => {
+  let num = 0;
+  const leftMap = {};
+  arr.forEach((v) => {
+    if (!leftMap[v]) {
+      leftMap[v] = 0;
+    }
+    leftMap[v]++;
+  });
+
+  const rightMap = {};
+  const lastElem = arr[arr.length - 1];
+  rightMap[lastElem] = 1;
+  leftMap[lastElem] -= 1;
+  for (let i = arr.length - 2; i > 0; i--) {
+    const middle = arr[i];
+    leftMap[middle]--;
+    const toTheRight = rightMap[middle * r] || 0;
+    const toTheLeft = leftMap[middle / r] || 0;
+    num += toTheRight * toTheLeft;
+    if (!rightMap[middle]) {
+      rightMap[middle] = 0;
+    }
+    rightMap[middle]++;
+  }
+  return num;
+};
+
+
+module.exports = { solve };
