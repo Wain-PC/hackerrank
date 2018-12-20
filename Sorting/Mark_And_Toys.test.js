01ff6ed442b87d5bed223512c78f9dbeab233a3b
Wain-PC <wain-pc@mail.ru>
2018-12-20 14:05:27 +0300

    Sorting/Mark_And_Toys

---
+++ Sorting/Mark_And_Toys.test.js
@@ -0,0 +1,7 @@
+const { solve } = require('./Mark_And_Toys');
+
+describe('Mark_And_Toys', () => {
+  it('should solve default example 1', () => {
+    expect(solve([1, 12, 5, 111, 200, 1000, 10], 50)).toBe(4);
+  });
+});
