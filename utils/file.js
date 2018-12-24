beb40f31cd788fd8b0c1f9bda4e007fc016b8276
Wain-PC <wain-pc@mail.ru>
2018-12-24 15:16:31 +0300

    Added small utility set to read long test data from files

---
+++ utils/file.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const { EOL } = require('os');
+
+const read = path => new Promise((resolve, reject) => {
+  fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
+    if (err) {
+      return reject(err);
+    }
+    return resolve(data);
+  });
+});
+
+const array = async (path) => {
+  const data = await read(path);
+  return data.split(EOL);
+};
+
+module.exports = { read, array };
