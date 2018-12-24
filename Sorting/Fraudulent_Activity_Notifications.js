b09309b74b11efead5b7aa6642f1f5c7398eef50
Wain-PC <wain-pc@mail.ru>
2018-12-24 15:15:41 +0300

    Sorting/Fraudulent_Activity_Notifications (counting sort)

--- Sorting/Fraudulent_Activity_Notifications.js
+++ Sorting/Fraudulent_Activity_Notifications.js
@@ -1,12 +1,45 @@
 const solve = (expenditure, d) => {
   let notifications = 0;
+  // Prepare the counting sort array.
+  const countSort = Array(201).fill(0);
+  for (let i = 0; i < d; i++) {
+    countSort[expenditure[i]] += 1;
+  }
+  // Prepare median calculation in countSort array
+  const middle = d / 2;
+  const isEven = d % 2 === 0;
+  const getMedian = () => {
+    let count = 0;
+    for (let i = 0; i < countSort.length; i++) {
+      count += countSort[i];
+      if (count >= middle) {
+        if (isEven) {
+          if (count === middle) {
+            // Continue cycling through the array to find next non-zero item
+            for (let j = i + 1; j < countSort.length; j++) {
+              if (countSort[j]) {
+                return (i + j) / 2;
+              }
+            }
+            return i;
+          }
+        }
+        return i;
+      }
+    }
+    return 0;
+  };
+
+  // For each value starting after the running window
   for (let i = d; i < expenditure.length; i++) {
     const current = expenditure[i];
-    const arr = expenditure.slice(i - d, i).sort((a, b) => a - b);
-    const median = d % 2 === 0 ? ((arr[(d / 2) - 1] + arr[d / 2]) / 2) : arr[(d / 2) - 0.5];
+    const median = getMedian();
     if (current >= 2 * median) {
       notifications++;
     }
+    // Use countSort as a queue-like object by manually shifting it to a new state.
+    countSort[expenditure[i - d]]--;
+    countSort[current]++;
   }
   return notifications;
 };
--- Sorting/Fraudulent_Activity_Notifications.test.js
+++ Sorting/Fraudulent_Activity_Notifications.test.js
@@ -8,4 +8,8 @@ describe('Fraudulent_Activity_Notifications', () => {
   it('should solve default example 2', () => {
     expect(solve([2, 3, 4, 2, 3, 6, 8, 4, 5], 5)).toBe(2);
   });
+
+  it('should solve default example 3', () => {
+    expect(solve([1, 5, 9, 15, 21, 28], 4)).toBe(2);
+  });
 });
