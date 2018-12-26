988545f252b51233ad82d8d46d10c513071ff90f
Wain-PC <wain-pc@mail.ru>
2018-12-26 15:59:26 +0300

    Strings/Making_Anagrams (better version)

--- Strings/Making_Anagrams.js
+++ Strings/Making_Anagrams.js
@@ -1,17 +1,12 @@
-const makeHash = str => str.split('').reduce((acc, letter) => {
-  if (!acc[letter]) {
-    acc[letter] = 0;
-  }
-  acc[letter]++;
-  return acc;
-}, {});
-
 const solve = (a, b) => {
-  const hash1 = makeHash(a);
-  const hash2 = makeHash(b);
-  return Object
-    .keys({ ...hash1, ...hash2 })
-    .reduce((acc, letter) => acc + Math.abs((hash1[letter] || 0) - (hash2[letter] || 0)), 0);
+  const arr = Array(26).fill(0);
+  const aCharCode = 'a'.charCodeAt(0);
+  const calc = (str, sign) => str.split('').forEach((letter) => {
+    arr[letter.charCodeAt(letter) - aCharCode] += sign;
+  });
+  calc(a, 1);
+  calc(b, -1);
+  return arr.reduce((acc, v) => acc + Math.abs(v), 0);
 };
 
 module.exports = { solve };
