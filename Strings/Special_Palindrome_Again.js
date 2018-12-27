972901680cd4413e4b684845658ed905deb44c20
Wain-PC <wain-pc@mail.ru>
2018-12-27 15:42:41 +0300

    Strings/Special_Palindrome_Again (1-pass solution)

---
+++ Strings/Special_Palindrome_Again.js
@@ -0,0 +1,32 @@
+const solve = (n, s) => {
+  let count = 0;
+  let current = '';
+  let same = 0;
+  const { length } = s;
+  for (let i = 0; i < length; i++) {
+    if (s[i] !== current) {
+      // 1+2+3+...+n
+      count += same * (same + 1) / 2;
+
+      // Special case for `aabaa` palindromes
+      let pal = 0;
+      for (let j = i + 1; j < i + same + 1; j++) {
+        if (current !== s[j]) {
+          break;
+        }
+        pal++;
+      }
+      count += pal;
+
+      same = 1;
+      current = s[i];
+    } else {
+      same++;
+    }
+  }
+  count += same * (same + 1) / 2;
+
+  return count;
+};
+
+module.exports = { solve };
---
+++ Strings/Special_Palindrome_Again.test.js
@@ -0,0 +1,43 @@
+const { solve } = require('./Special_Palindrome_Again');
+
+describe('Alternating_Characters', () => {
+  it('should solve default example 1', () => {
+    const s = 'asasd';
+    expect(solve(s.length, s)).toBe(7);
+  });
+
+  it('should solve default example 2', () => {
+    const s = 'abcbaba';
+    expect(solve(s.length, s)).toBe(10);
+  });
+
+  it('should solve default example 3', () => {
+    const s = 'aaaa';
+    expect(solve(s.length, s)).toBe(10);
+  });
+
+  it('should solve default example 4', () => {
+    const s = 'cbbabbc';
+    expect(solve(s.length, s)).toBe(11);
+  });
+
+  it('should solve default example 4', () => {
+    const s = 'mnonopoo';
+    expect(solve(s.length, s)).toBe(12);
+  });
+
+  it('should solve default example 5', () => {
+    const s = 'mmmmmmbmmmmmm';
+    expect(solve(s.length, s)).toBe(49);
+  });
+
+  it('should solve default example 6', () => {
+    const s = 'mmmmmmbmm';
+    expect(solve(s.length, s)).toBe(27);
+  });
+
+  it('should solve default example 7', () => {
+    const s = 'qwertyuiop';
+    expect(solve(s.length, s)).toBe(s.length);
+  });
+});
