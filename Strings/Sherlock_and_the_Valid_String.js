f9532d232ba985efc5e619052ba878cbdd8b8957
Wain-PC <wain-pc@mail.ru>
2018-12-28 14:37:32 +0300

    Strings/Sherlock_and_the_Valid_String

---
+++ Strings/Sherlock_and_the_Valid_String.js
@@ -0,0 +1,33 @@
+const solve = (s) => {
+  const hash = {};
+  const { length } = s;
+  for (let i = 0; i < length; i++) {
+    const letter = s[i];
+    if (!hash[letter]) {
+      hash[letter] = 0;
+    }
+    hash[letter]++;
+  }
+  const arr = Object.values(hash);
+  const mainFreq = arr[0];
+  let decrementUsed = false;
+  for (let i = 0; i < arr.length; i++) {
+    const diff = Math.abs(arr[i] - mainFreq);
+    if (diff > 1) {
+      if (arr[i] === 1 && !decrementUsed) {
+        decrementUsed = true;
+      } else {
+        return 'NO';
+      }
+    }
+    if (diff === 1) {
+      if (decrementUsed) {
+        return 'NO';
+      }
+      decrementUsed = true;
+    }
+  }
+  return 'YES';
+};
+
+module.exports = { solve };
