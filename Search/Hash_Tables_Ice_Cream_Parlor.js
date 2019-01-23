ef3b0f77936d7f0f40600faeef7e7051342401d0
Wain-PC <wain-pc@mail.ru>
2019-01-23 14:02:30 +0300

    Search/Hash_Tables_Ice_Cream_Parlor (binary search way)

--- Search/Hash_Tables_Ice_Cream_Parlor.js
+++ Search/Hash_Tables_Ice_Cream_Parlor.js
@@ -1,4 +1,4 @@
-const solve = (cost, money) => {
+const solveHash = (cost, money) => {
   const hash = cost.reduce((acc, price, index) => {
     if (!acc[price]) {
       acc[price] = [];
@@ -19,4 +19,37 @@ const solve = (cost, money) => {
   return output.sort((a, b) => a - b);
 };
 
-module.exports = { solve };
+const solveBinary = (cost, money) => {
+  const indexedCost = cost
+    .map((c, i) => ({ cost: c, index: i + 1 }))
+    .sort((a, b) => a.cost - b.cost);
+  // Implement binary search on the sorted array
+  const binary = (arr, value) => {
+    let start = 0;
+    let end = arr.length - 1;
+    while (start < end) {
+      const middle = Math.floor(end - start / 2);
+      const c = arr[middle].cost;
+      if (c === value) {
+        return arr[middle];
+      }
+      // Value should be in the left part of the array
+      if (value < c) {
+        end = middle - 1;
+      } else {
+        start = middle + 1;
+      }
+    }
+    return null;
+  };
+
+  for (let i = 0; i < indexedCost.length; i++) {
+    const pair = binary(indexedCost, money - indexedCost[i].cost);
+    if (pair) {
+      return [indexedCost[i].index, pair.index];
+    }
+  }
+  return [];
+};
+
+module.exports = { solveBinary, solveHash };
--- Search/Hash_Tables_Ice_Cream_Parlor.test.js
+++ Search/Hash_Tables_Ice_Cream_Parlor.test.js
@@ -1,11 +1,18 @@
-const { solve } = require('./Hash_Tables_Ice_Cream_Parlor');
+const { solveBinary, solveHash } = require('./Hash_Tables_Ice_Cream_Parlor');
 
 describe('Hash_Tables_Ice_Cream_Parlor', () => {
   it('should solve default example 1', () => {
-    expect(solve([1, 4, 5, 3, 2], 4)).toEqual([1, 4]);
+    expect(solveBinary([1, 4, 5, 3, 2], 4)).toEqual([1, 4]);
+    expect(solveHash([1, 4, 5, 3, 2], 4)).toEqual([1, 4]);
   });
 
   it('should solve default example 2', () => {
-    expect(solve([2, 2, 4, 3], 4)).toEqual([1, 2]);
+    expect(solveBinary([2, 2, 4, 3], 4)).toEqual([1, 2]);
+    expect(solveHash([2, 2, 4, 3], 4)).toEqual([1, 2]);
+  });
+
+  it('should solve default example 3', () => {
+    expect(solveBinary([1, 2, 3, 5, 6], 5)).toEqual([2, 3]);
+    expect(solveHash([1, 2, 3, 5, 6], 5)).toEqual([2, 3]);
   });
 });
