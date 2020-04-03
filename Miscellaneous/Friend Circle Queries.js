36046aee38179947d689818410d6f60126d6d7b0
Pavel Dranichnikov <psdranichnikov@avito.ru>
2020-04-03 20:16:11 +0300

    Miscellaneous/Friend Circle Queries (better implementation, still doesn't pass time constraints)

--- Miscellaneous/Friend Circle Queries.js
+++ Miscellaneous/Friend Circle Queries.js
@@ -50,4 +50,78 @@ const solve = (queries) => {
   return maxCircles;
 };
 
-module.exports = { solve };
+// Better algorithm.
+// 3 case
+const solveBetter = (queries) => {
+  const people = {};
+  const circles = [];
+  const maxCircles = [];
+
+  const addFriends = (p1, p2) => {
+    // Case 1. Both friends are NOT present.
+    // Add them to people list and create a new circle.
+    if (!people[p1] && !people[p2]) {
+      const circleId = circles.length;
+      people[p1] = { circleId };
+      people[p2] = { circleId };
+      circles.push([p1, p2]);
+      // Case 2.1 First person is in circle, other is not.
+      // We should append p2 to the existing circle.
+    } else if (people[p1] && !people[p2]) {
+      const { circleId } = people[p1];
+      people[p2] = { circleId };
+      circles[circleId].push(p2);
+      // Case 2.2 First person is NOT in circle, other is in.
+      // Exactly the same as 2.1, but people are swapped
+    } else if (!people[p1] && people[p2]) {
+      const { circleId } = people[p2];
+      people[p1] = { circleId };
+      circles[circleId].push(p1);
+      // Case 3. Both are in circles.
+      // If the circle is the same, it's a noop.
+    } else {
+      const circleId1 = people[p1].circleId;
+      const circleId2 = people[p2].circleId;
+      // If circleIds are the same, it's a noop
+      if (circleId1 === circleId2) {
+        // noop
+        return;
+      }
+
+      const circle1 = circles[circleId1];
+      const circle2 = circles[circleId2];
+      // Move everyone from the smaller circle to the bigger one.
+      // Then change circleId for every changed id.
+      if (circle1.length > circle2.length) {
+        circles[circleId1] = circle1.concat(circle2);
+        circle2.forEach((person) => {
+          // eslint-disable-next-line no-param-reassign
+          people[person].circleId = circleId1;
+        });
+        circles[circleId2] = undefined;
+      } else {
+        circles[circleId2] = circle2.concat(circle1);
+        circle1.forEach((person) => {
+          // eslint-disable-next-line no-param-reassign
+          people[person].circleId = circleId2;
+        });
+        circles[circleId1] = undefined;
+      }
+    }
+  };
+
+  queries.forEach(([p1, p2]) => {
+    // Step 1. Add new people to the hash.
+    addFriends(p1, p2);
+
+    // Step 2. Select max circle length of the current step.
+    const max = circles
+      .filter(c => c !== undefined)
+      .reduce((acc, c) => (c.length > acc ? c.length : acc), 0);
+    maxCircles.push(max);
+  });
+
+  return maxCircles;
+};
+
+module.exports = { solve: solveBetter };
--- Miscellaneous/Friend Circle Queries.test.js
+++ Miscellaneous/Friend Circle Queries.test.js
@@ -2,12 +2,35 @@ const { solve } = require('./Friend Circle Queries');
 
 describe('Friend Circle Queries', () => {
   const examples = [
-
     {
-      query: [[1, 2], [1, 3]],
+      query: [
+        [6, 4],
+        [5, 9],
+        [8, 5],
+        [4, 1],
+        [1, 5],
+        [7, 2],
+        [4, 2],
+        [7, 6],
+      ],
+      answer: [2, 2, 3, 3, 6, 6, 8, 8],
+    },
+    {
+      query: [
+        [1, 2],
+        [1, 3],
+      ],
       answer: [2, 3],
     },
-
+    {
+      query: [
+        [1000000000, 23],
+        [11, 3778],
+        [7, 47],
+        [11, 1000000000],
+      ],
+      answer: [2, 2, 2, 4],
+    },
   ];
 
   examples.forEach(({ query, answer }, i) => {
