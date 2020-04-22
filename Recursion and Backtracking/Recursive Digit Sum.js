74eb5c9b182358c0f8b31fc41539ec442c883026
Pavel Dranichnikov <psdranichnikov@avito.ru>
2020-04-22 19:24:29 +0300

    Recursion and Backtracking/Recursive Digit Sum

--- Recursion and Backtracking/Recursive Digit Sum.js
+++ Recursion and Backtracking/Recursive Digit Sum.js
@@ -1,15 +1,12 @@
-const superDigit = (str) => {
-  if (str.length === 1) {
+const superDigit = (str, repeats) => {
+  const { length } = str;
+  if (length === 1) {
     return str;
   }
-
-  const result = str.split('').reduce((acc, n) => acc + +n, 0);
-  return superDigit(`${result}`);
+  const result = str.split('').reduce((acc, n) => acc + +n, 0) * repeats;
+  return superDigit(`${result}`, 1);
 };
 
-const solve = (n, k) => {
-  const p = `${n}`.repeat(k);
-  return +superDigit(p);
-};
+const solve = (n, k) => +superDigit(`${n}`, k);
 
 module.exports = { solve };
