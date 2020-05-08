051e81e8d8f909567143a041c345858795875551
Pavel Dranichnikov <psdranichnikov@avito.ru>
2020-05-08 20:26:28 +0300

    Recursion and Backtracking/Candies (pass 1 more test)

--- Dynamic Programming/Candies.js
+++ Dynamic Programming/Candies.js
@@ -13,13 +13,15 @@ const solve = (children) => {
     return 0;
   }
 
+  const candies = [];
+
   const getCandies = memo((index) => {
     if (index < 0 || index >= children.length) {
       return 0;
     }
     // If it's a local maximum, we should take max of left and right and increase it by 1.
     if ((children[index] > children[index - 1]) && (children[index] > children[index + 1])) {
-      return Math.max(getCandies(index - 1), getCandies(index + 1)) + 1;
+      return Math.max(candies[index - 1], getCandies(index + 1)) + 1;
     }
 
     // Decreasing trend. Current should be 1 more than next.
@@ -29,7 +31,7 @@ const solve = (children) => {
 
     // Increasing trend. Current should 1 more than previous.
     if (children[index] > children[index - 1]) {
-      return getCandies(index - 1) + 1;
+      return candies[index - 1] + 1;
     }
 
     // Local minimum or same value as previous. Should always return 1.
@@ -37,8 +39,13 @@ const solve = (children) => {
   });
 
 
-  const candies = children.map((value, index) => getCandies(index));
-  return candies.reduce((acc, value) => acc + value, 0);
+  return children.reduce((acc, value, index) => {
+    if (!candies[index]) {
+      candies[index] = getCandies(index);
+    }
+
+    return acc + candies[index];
+  }, 0);
 };
 
 module.exports = { solve };
