89c08dfe4c2d01af2a658c71eedf112fd3c78492
Pavel Dranichnikov <psdranichnikov@avito.ru>
2020-05-11 22:14:53 +0300

    Stacks and Queues/Queues: A Tale of Two Stacks

---
+++ Stacks and Queues/Queues: A Tale of Two Stacks.js
@@ -0,0 +1,65 @@
+class Queue {
+  constructor() {
+    this.input = [];
+    this.output = [];
+  }
+
+  enqueue(value) {
+    this.input.push(value);
+  }
+
+  prepare() {
+    if (!this.output.length) {
+      while (this.input.length) {
+        this.output.push(this.input.pop());
+      }
+    }
+  }
+
+  dequeue() {
+    this.prepare();
+    return this.output.pop();
+  }
+
+  top() {
+    this.prepare();
+
+    if (!this.output.length) {
+      return null;
+    }
+
+    return this.output[this.output.length - 1];
+  }
+}
+
+const processLine = (line, queue) => {
+  const [operation, value] = line.split(' ');
+
+  switch (operation) {
+    case '1': {
+      queue.enqueue(value);
+      return null;
+    }
+    case '2': {
+      queue.dequeue();
+      return null;
+    }
+    case '3': {
+      return queue.top();
+    }
+    default: {
+      return null;
+    }
+  }
+};
+
+const solve = (input) => {
+  const queue = new Queue();
+  return input
+    .split('\n')
+    .slice(1)
+    .map(line => processLine(line, queue))
+    .filter(v => v !== null);
+};
+
+module.exports = { solve };
